e0cdeb734b82c696cf24f9342d274d88a65b6daa
Jakob Borg <jakob@nym.se>
2012-03-19 12:28:21 +0100

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Deploy to where?
+DIR=/opt/www/snooze
+
+# Move the previous deployment to a backup
+rm -rf $DIR.prev
+mv $DIR $DIR.prev
+
+mkdir -p $DIR
+git archive HEAD | tar xf - -C $DIR
+
+# Create a deployment description file
+VERS=`git describe --always`
+DATE=`git log -n 1 --pretty=%ai`
+echo "var SNOOZE_VERSION='$VERS';" > $DIR/deploydata.js
+echo "var SNOOZE_DATE='$DATE';" >> $DIR/deploydata.js
+
+# Update cache manifest so the app is reloaded
+echo >> $DIR/cache.manifest
+echo "# Version: $VERS" >> $DIR/cache.manifest
+echo "# Date: $DATE" >> $DIR/cache.manifest
+
